db7c0de58a70a5acf42a04a2892f971e05404f44
zannujulius <zannujulius14@gmail.com>
2022-03-18 15:24:06 +0100

    update the image og

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -35,35 +35,23 @@ export default class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link rel="stylesheet" href="./style.css" />
-                    <meta name="msapplication-TileImage" content="https://friendly-saha-49ec00.netlify.app/imgs/doubleDIce.png"/> 
+                    <meta name="msapplication-TileImage" content="https://images.unsplash.com/photo-1640622660914-4b56c79bb492?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"/> 
                     <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Russo+One&display=swap" rel="stylesheet" />
 
                     <meta property="og:description" content="Welcome to double dice betting website" />
-
                     <meta property="og:site_name" content="Double Dice betting"/>
                     <meta property="og:title" content="Double Dice betting" />
 
-                    <meta name="og:image" itemProp="image" content='https://friendly-saha-49ec00.netlify.app/imgs/doubleDIce.png'/>
-
-                    {/* <meta property="og:type" content="website" /> */}
-                    <meta property="og:type" content="Double Dice betting" />
-
+                    <meta name="og:image" itemProp="image" content='https://images.unsplash.com/photo-1640622660914-4b56c79bb492?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'/>
+                    <meta property="og:type" content="website" />
                     <meta property="og:image:type" content="image/jpeg"/>
-
-                    <meta property="og:URL" content="https://friendly-saha-49ec00.netlify.app/" />
-
-                    <meta property="og:image:type" content="image/png" />
+                    <meta property="og:URL" content="https://doubledicebet.herokuapp.com/" />
                     <meta property="og:image:width" content="200" />
                     <meta property="og:image:height" content="200" />
                     <meta property="og:image:alt" content="double dice image" />
-
-                    {/* <meta property="og:title" name="twitter:title" content="Double Dice betting" /> */}
-                    {/* <meta property="og:description" name="twitter:description" content="Welcome to double dice betting website" /> */}
-                    {/* <meta name="twitter:card" content="summary" /> */}
-                    {/* <meta property="og:image" name="twitter:image" content="https://images.unsplash.com/photo-1640622660914-4b56c79bb492?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" /> */}
                 </Head>
                 <body>
                     <Main />
